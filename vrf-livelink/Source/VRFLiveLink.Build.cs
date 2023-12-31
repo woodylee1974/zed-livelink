1bef8b71d8eea49420dfbc40b7bd8ea70313f225
li.woodyli <li.woodyli@gmail.com>
2023-12-31 21:33:13 +0800

    in progressing

---
+++ vrf-livelink/Source/VRFLiveLink.Build.cs
@@ -0,0 +1,40 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+using System;
+
+public class VRFLiveLink : ModuleRules
+{
+	public VRFLiveLink(ReadOnlyTargetRules Target) : base(Target)
+	{
+		bEnableUndefinedIdentifierWarnings = false;
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			//Do nothing
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Linux)
+		{
+			//Do nothing
+		}
+
+		string WrapperPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "../lib/"));
+
+		PrivateIncludePaths.AddRange(new string[] { "Runtime/Launch/Public", "Runtime/Launch/Private" });
+
+		// Unreal dependency modules
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"ApplicationCore",
+			"Projects",
+			"UdpMessaging",
+			"LiveLinkInterface",
+			"LiveLinkMessageBusFramework",
+		});
+
+	}
+}
+
